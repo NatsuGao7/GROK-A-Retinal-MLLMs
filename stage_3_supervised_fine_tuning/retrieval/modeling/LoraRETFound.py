63fc0161249867958e8a9246a6a71d77963c471d
Hongyi Qin <113676973+Hongyi311@users.noreply.github.com>
2025-08-12 05:45:11 +0100

    Add files via upload

---
+++ stage_3_supervised_fine_tuning/retrieval/modeling/LoraRETFound.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+from .models_vit import VisionTransformer
+from functools import partial
+from .pos_embed import interpolate_pos_embed
+from .timm_models.models.layers import trunc_normal_
+class Identity(nn.Module):
+    """Identity layer to replace last fully connected layer"""
+    def forward(self, x):
+        return x
+    
+def vit_large_patch16(pretrained=True):
+    model = VisionTransformer(
+        patch_size=16, embed_dim=1024, depth=24, num_heads=16, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6))
+    # manually initialize fc layer
+    trunc_normal_(model.head.weight, std=2e-5)
+    # print("Model = %s" % str(model))
+    model.head = Identity()
+    return model
+
+
+from retrieval.modeling.LORA.lora_image_encoder import LoRA_ViT
+def lora(pretrained=True,R=8):
+    ViT= vit_large_patch16(pretrained=pretrained)
+    model = LoRA_ViT(ViT, r=R)
+    return model
+
+
+if __name__ == "__main__":
+    net = vit_large_patch16(pretrained=False)
+    # print("num_features == {}".format(num_features))
+    # print("num_features_l == {}".format(num_features_l))
+    images = torch.rand(2, 3, 224, 224)
+    output = net(images)
+    print(output.shape)
